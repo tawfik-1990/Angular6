c0eb0bbaff34613260345336985994cae69b8de4
Bouguerba, Tawfik <tawfik.bouguerba@stud.hs-ruhrwest.de>
2019-06-12 12:09:30 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { IntroductionComponent } from './introduction/introduction.component';
+import { ContentComponent } from './content/content.component';
+import { TestimonialComponent } from './testimonial/testimonial.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ClientsComponent } from './clients/clients.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { CommonModule } from '@angular/common';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'Home', pathMatch: 'full' },
+  
+  { path:'Home', component: HeaderComponent},
+  { path: 'About', component: IntroductionComponent },
+  { path: 'Services', component: ContentComponent  },
+  { path: 'Testimonials', component: TestimonialComponent  },
+  { path: 'Gallery', component: GalleryComponent },
+  { path: 'Clients', component:  ClientsComponent  },
+  { path: 'Pricing', component: PricingComponent },
+];
+ 
+  
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [CommonModule,  RouterModule.forRoot(routes,{ enableTracing: true } )],
+  declarations:[],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IntroductionComponent } from './introduction/introduction.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ContentComponent } from './content/content.component';
+import { TestimonialComponent } from './testimonial/testimonial.component';
+import { FooterComponent } from './footer/footer.component';
+import { ClientsComponent } from './clients/clients.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { HeaderComponent } from './header/header.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { SocialComponent } from './social/social.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroductionComponent,
+    GalleryComponent,
+    ContentComponent,
+    TestimonialComponent,
+    FooterComponent,
+    ClientsComponent,
+    PricingComponent,
+    HeaderComponent,
+    NavigationComponent,
+    SocialComponent
   ],
   imports: [
     BrowserModule,
